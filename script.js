f95109575dea2a2f058f21830b421151fc1829a2
Zach Watkins <zwatkins31@gmail.com>
2023-01-11 16:14:17 -0800

    final commit

--- script.js
+++ script.js
@@ -1,14 +1,75 @@
+const audioContext = new AudioContext();
+
+// Global object
 const NOTE_DETAILS = [
-  { note: "C", key: "Z", frequency: 261.626 },
-  { note: "Db", key: "S", frequency: 277.183 },
-  { note: "D", key: "X", frequency: 293.665 },
-  { note: "Eb", key: "D", frequency: 311.127 },
-  { note: "E", key: "C", frequency: 329.628 },
-  { note: "F", key: "V", frequency: 349.228 },
-  { note: "Gb", key: "G", frequency: 369.994 },
-  { note: "G", key: "B", frequency: 391.995 },
-  { note: "Ab", key: "H", frequency: 415.305 },
-  { note: "A", key: "N", frequency: 440 },
-  { note: "Bb", key: "J", frequency: 466.164 },
-  { note: "B", key: "M", frequency: 493.883 },
+  { note: "C", key: "Z", frequency: 261.626, active: false },
+  { note: "Db", key: "S", frequency: 277.183, active: false },
+  { note: "D", key: "X", frequency: 293.665, active: false },
+  { note: "Eb", key: "D", frequency: 311.127, active: false },
+  { note: "E", key: "C", frequency: 329.628, active: false },
+  { note: "F", key: "V", frequency: 349.228, active: false },
+  { note: "Gb", key: "G", frequency: 369.994, active: false },
+  { note: "G", key: "B", frequency: 391.995, active: false },
+  { note: "Ab", key: "H", frequency: 415.305, active: false },
+  { note: "A", key: "N", frequency: 440, active: false },
+  { note: "Bb", key: "J", frequency: 466.164, active: false },
+  { note: "B", key: "M", frequency: 493.883, active: false },
 ];
+
+document.addEventListener("keydown", (e) => {
+  if (e.repeat) return; // guard clause
+  const keyboardKey = e.code;
+  const noteDetail = getNoteDetail(keyboardKey);
+  //   console.log(noteDetail);
+
+  if (noteDetail == null) return; // if we don't have a not detail we exit function
+
+  // when we press down on a key, we mark our note as active, then call the playNotes function
+  noteDetail.active = true;
+  playNotes();
+});
+
+document.addEventListener("keyup", (e) => {
+  const keyboardKey = e.code;
+  const noteDetail = getNoteDetail(keyboardKey);
+
+  if (noteDetail == null) return;
+
+  noteDetail.active = false;
+  playNotes();
+});
+
+function getNoteDetail(keyboardKey) {
+  return NOTE_DETAILS.find((n) => `Key${n.key}` === keyboardKey); // 'n' for note
+}
+
+function playNotes() {
+  // Apply css classes to active keys (ie keys being pressed)
+  NOTE_DETAILS.forEach((n) => {
+    const keyElement = document.querySelector(`[data-note="${n.note}"]`);
+    keyElement.classList.toggle("active", n.active);
+    // If we have a current oscillator on the note, we stop playing it - that way we don't have multiple ocsillators playing for the same note
+    if (n.oscillator != null) {
+      n.oscillator.stop();
+      n.oscillator.disconnect();
+    }
+  });
+
+  // Get active notes, loop through each one and start playing the note (ie call startNote() )
+  const activeNotes = NOTE_DETAILS.filter((n) => n.active);
+  const gain = 1 / activeNotes.length;
+  activeNotes.forEach((n) => {
+    startNote(n, gain);
+  });
+}
+
+function startNote(noteDetail, gain) {
+  const gainNode = audioContext.createGain();
+  gainNode.gain.value = gain;
+  const oscillator = audioContext.createOscillator();
+  oscillator.frequency.value = noteDetail.frequency;
+  oscillator.type = "sine";
+  oscillator.connect(audioContext.destination);
+  oscillator.start();
+  noteDetail.oscillator = oscillator;
+}
